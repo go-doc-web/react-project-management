0c6611129715d14c4e2244dff8e9f4442c3ed87f
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-05-29 15:13:28 +0300

    create NewProject component

--- src/componets/layout/Layout.jsx
+++ src/componets/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ProjectSidebar from "../ProjectSidebar";
 import NewProject from "../NewProject";
+import NoProjectSelected from "../NoProjectSelected";
 
 const Layout = () => {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
       <NewProject />
+      <NoProjectSelected />
     </main>
   );
 };
